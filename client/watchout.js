64501988cc18d7d840a01341613b21cc4ce88b77
BKhurshid <josephsun4@gmail.com>
2015-10-17 02:45:24 -0700

    Added a circle and made the circle draggable. Just need to make it aware of when it hits a asteroid. Also need to add and delete points

--- client/watchout.js
+++ client/watchout.js
@@ -31,16 +31,6 @@ var createNode = function(){
 initialize();//calls initialize to get all thirty asteroids on the browser.
 
 
-/*
-line 37-43 append a circle class to the main svg element.
-*/
-d3.select('.svgTag').append('circle')
-  // .attr('height', 25)
-  // .attr('width', 25)
-  .attr("class", 'circle')
-  .attr('cx', '25')
-  .attr('cy', '25')
-  .attr('r', '15');
 
 
 /*
@@ -71,22 +61,56 @@ for (var i = 0; i < 30; i++) {
 The function moveEnemies randomly changes each and every asteroids position
 */
 var moveEnemies = function() {
-  d3.selectAll('.enemies')
+  d3.selectAll('.enemies')  
   .data(enemyArray)
   .transition().duration(1500) 
   .attr('x', function(d) {
-    if (d.x < 100) {
-      d.x =  500;
+    if (d.x < 100) {// if the asteroid is located close to the left of the screen
+      d.x =  500;// move them closer to the right.
     }
     return d.x * Math.random();
   })
   .attr('y', function(d) {
-    if (d.y < 100) {
-      d.y = 400
+    if (d.y < 100) { // if the asteroid is located close to the top of the screen.
+      d.y = 400// move them closer to the bottom
     }
     return d.y * Math.random();
   })
 };
+/*
+line 37-43 append a circle tag to the main svg element.
+*/
+d3.select('.svgTag').append('circle')
+  // .attr('height', 25)
+  // .attr('width', 25)
+  .attr("class", 'pow')
+  .attr('cx', '25')
+  .attr('cy', '25')
+  .attr('r', '15');
+
+/*
+The mover function changes the positioning of cx and cy to the event (draging)
+position. (?)
+*/
+function mover () {
+  d3.select('circle')
+    .attr("cx", d3.event.x )
+    .attr("cy", d3.event.y) ;
+}
+/*
+Not too sure how drag variable works. d3.beahvior.drag() creates an object???
+.on('drag', mover) listens for when someone drags the circle and then calls mover 
+when they do.
+*/
+var drag = d3.behavior.drag()
+    .on("drag", mover);
+
+/*
+calls drag on the circle tag (object? d3 node? )
+I sincerly dislike using libraries without understanding whats under the hood. 
+*/
+d3.select('body').select('svg').select("circle")
+      .call(drag);
 /*
 call moveEnemies every second.
 */
