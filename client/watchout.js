2d7f878142b227c834fa365a135a908e441ca0ca
JosephSun <bazzkhurshid@yahoo.com>
2015-10-17 11:48:40 -0700

    Made it a bit more readable by changing var drag to var listenerForDrag

--- client/watchout.js
+++ client/watchout.js
@@ -10,13 +10,16 @@ var initialize = function () {
   for (var j = 0; j < 30; j++) {
     createNode();
   }  
-}
+};
 /*
 The function createNode appends a svg:image tag to all svg tags (there is only one
 svg tag as of now.). The function also sets the class of that svg:image tag to enemies, 
 the xlink:href to an asteroid picture, the width to 50, the height to 50, the x and y value of the object
 associated with the tag to random
 */
+
+//go on debugger to check how x and y are being added and where they are being added. Marc Answer: Educated guess is that these are manipulating the 
+//d3 object node and it is abstracting (under the hood) to affect the dom node. 
 var createNode = function(){
     d3.select("svg").append('svg:image')
         .attr("class", "enemies")
@@ -26,7 +29,8 @@ var createNode = function(){
         // .attr("position", 'absolute')
         .attr("x", 900 * Math.random())
         .attr("y", 800 * Math.random());
-  }
+        debugger;
+  };
   
 initialize();//calls initialize to get all thirty asteroids on the browser.
 
@@ -49,6 +53,7 @@ var newEnemy = function (x, y){
 creates a global array that holds all of the enemies. 
 */
 var enemyArray = [];
+
 /*
 this for loop pushes all of the enemies into the enemyArray
 */
@@ -100,21 +105,21 @@ function mover () {
     .attr("cy", d3.event.y) ;
     yCirclePosition = d3.event.y;
     xCirclePostion = d3.event.x;
-}
+};
 /*
 Not too sure how drag variable works. d3.beahvior.drag() creates an object???
 .on('drag', mover) listens for when someone drags the circle and then calls mover 
 when they do.
 */
-var drag = d3.behavior.drag()
-    .on("drag", mover);
+var listenerForDrag = d3.behavior.drag() //Come back and read api of behavior and/or drag()
+    .on("drag", mover); 
 
 /*
 calls drag on the circle tag (object? d3 node? )
 I sincerly dislike using libraries without understanding whats under the hood. 
 */
 d3.select('body').select('svg').select("circle")
-      .call(drag);
+      .call(listenerForDrag);
 
 
 var highScore = 0;
@@ -131,13 +136,13 @@ collision count;
 var pointsToZero = function() {
   currentScore++;
   d3.select('.cScore').text("" + currentScore);
-
+  var xDistance;
+  var yDistance;
   for (var e = 0; e < enemyArray.length; e++) {
     xDistance = enemyArray[e].x - xCirclePostion;
     yDistance = enemyArray[e].y - yCirclePosition;
     if (xDistance < 0.8 && xDistance > -0.8 || yDistance < 0.8 && yDistance > - 0.8) {
       setScoreToZero();
-      collisions++;
     }
   }
 };  
@@ -147,6 +152,7 @@ setScoreToZero function sets current score to HScore if greater. It changes the
 text of Collisions, CScore and HScore. 
 */
 var setScoreToZero = function (){
+  collisions++;
   if (currentScore > highScore) {
     highScore = currentScore;
     d3.select('.hScore').text("" + highScore);
